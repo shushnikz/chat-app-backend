f17941c4dc277516e76462175fe7dc0bd359e045
shushnikz <sushmithadevadiga997@gmail.com>
2023-01-14 18:21:00 +0530

    added home page

--- connection.js
+++ connection.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
+mongoose.set('strictQuery', false);
 mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PW}@cluster0.4ismyhv.mongodb.net/chat-app`, () => {
     console.log("DB connected")
 })
