5aece94ef266238d5d9eb4bbb002b759fa539881
shushnikz <sushmithadevadiga997@gmail.com>
2023-01-14 14:46:28 +0530

    added mongo

--- connection.js
+++ connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-mongoose.connect(`mongodb://${process.env.DB_USER}:${process.env.DB_PW}@ac-1zsaov0-shard-00-00.4ismyhv.mongodb.net:27017,ac-1zsaov0-shard-00-01.4ismyhv.mongodb.net:27017,ac-1zsaov0-shard-00-02.4ismyhv.mongodb.net:27017/?ssl=true&replicaSet=atlas-w1rvdu-shard-0&authSource=admin&retryWrites=true&w=majority`, () => {
+mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PW}@cluster0.4ismyhv.mongodb.net/chat-app`, () => {
     console.log("DB connected")
 })
